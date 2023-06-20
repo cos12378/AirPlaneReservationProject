0c9af4592e50b4ba4515e0e5533f693aba13189b
icebear2n2 <icebear2n2@gmail.com>
2023-06-20 17:47:05 +0900

    메인 페이지 추가

--- src/main/java/Servlet/ReservationServlet.java
+++ src/main/java/Servlet/ReservationServlet.java
@@ -1,29 +1,30 @@
 package Servlet;
 import Module.ModuleManager;
-import Module.*;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import Module.*;
 
-public class MainServlet extends HttpServlet{
+public class ReservationServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
-        //main 진입 시 모듈 메니저 초기화
+        // main 진입 시 모듈 메니저 초기화
         ModuleManager.getInstance().initModuleManager();
         super.init();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("views/main.html").forward(req,resp);
         super.doGet(req, resp);
+        ReservationModule module = ModuleManager.getInstance().getReservationModuleByNowMobule();
+
+        module.getAirPlaneList_by_Database();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        MainModule module = ModuleManager.getInstance().getMainModuleByNowMobule();
-
-
         super.doPost(req, resp);
     }
 }
