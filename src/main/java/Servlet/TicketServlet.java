5f061e9017da578a14a4692e89dfc22d92050d34
icebear2n2 <icebear2n2@gmail.com>
2023-06-24 19:06:40 +0900

    주석 추가

--- src/main/java/Servlet/TicketServlet.java
+++ src/main/java/Servlet/TicketServlet.java
@@ -19,21 +19,31 @@ public class TicketServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // ModuleManager 의 인스턴스를 얻어온 후, changeModule 메서드를 호출하여 RESERVATION 모듈로 변경
         ModuleManager.getInstance().changeModule(ModuleType.RESERVATION);
+
+        // ModuleManager 의 인스턴스를 얻어온 후, getReservationModuleByNowModule 메서드를 호출
+        // 현재 모듈의 ReservationModule 인스턴스를 가져온다
+        // module 변수에 현재 실행 중인 예약 모듈의 인스턴스가 저장된다.
         ReservationModule module = ModuleManager.getInstance().getReservationModuleByNowMobule();
-        resp.addCookie(new Cookie("LoginID", "test"));
+
+
+//        resp.addCookie(new Cookie("LoginID", "test"));
+
+        // 요청에 포함된 쿠키들 중에서 이름이 "LoginID"인 쿠키의 값을 가져와 userID 변수에 저장
         Cookie[] cookies = req.getCookies();
         String userID = "";
+
         for (int i = 0; i < cookies.length; i++) {
             if (cookies[i].getName().equals("LoginID"))
                 userID = cookies[i].getValue();
         }
+
+        // getMyTickets_by_Database() 메서드를 실행하여 데이터베이스로부터 비행기 티켓 목록을 가져와 데이터를 반환
         req.setAttribute("ticketList", module.getMyTickets_by_Database(userID));
+
+        // 현재 요청을 ticketList.jsp로 전달
         req.getRequestDispatcher("views/ticketList.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
